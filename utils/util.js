e3a47383c50d9d8d6c6eca72c154256470b5324a
moshangongzi <2061935864@qq.com>
2022-05-29 13:58:01 +0800

    完成了首页新闻列表和新闻详细内容

--- utils/util.js
+++ utils/util.js
@@ -6,14 +6,26 @@ const formatTime = date => {
   const minute = date.getMinutes()
   const second = date.getSeconds()
 
-  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
   n = n.toString()
-  return n[1] ? n : `0${n}`
+  return n[1] ? n : '0' + n
+}
+
+/**
+ * 匹配 body 中的元素，删除 script 标签
+ */
+
+var REG_BODY = /<body[^>]*>([\s\S]*)<\/body>/
+const getBodyHtml = html => {
+  let result = REG_BODY.exec(html)
+  if (result && result.length === 2) return result[1].replace(/<script.*?>.*?<\/script>/gi, '')
+  return content.replace(/<script.*?>.*?<\/script>/gi, '')
 }
 
 module.exports = {
+  getBodyHtml,
   formatTime
 }
