477438c793dd2814138429b1a1175d3b962a1748
Amy <2503360709@qq.com>
2022-06-04 10:13:03 +0800

    收藏功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,9 @@
 const App = getApp()
 Page({
     data: {
+        openid:'',
+        aid:'',
+        actArr:[],
         headerTitleName: [
             { name: '推荐', id: 1 },
             { name: '活动', id: 2 },
@@ -15,141 +18,127 @@ Page({
         videoList: [],
         randomArray: [],
         categoryList: [{
-                id: 1,
-                firstCateName: '广场舞',
-                secondCate: [{
-                        id: '1_1',
-                        name: '16步',
-                    },
-                    {
-                        id: '1_2',
-                        name: '32步'
-                    },
-                    {
-                        id: '1_3',
-                        name: '步子舞'
-                    },
-                    {
-                        id: '1_4',
-                        name: '中三舞'
-                    }
-                ]
+            id: 1,
+            firstCateName: '广场舞',
+            secondCate: [{
+                id: '1_1',
+                name: '16步',
             },
             {
-                id: 2,
-                firstCateName: '健身',
-                secondCate: [{
-                        id: '2_1',
-                        name: '健身操',
-                    },
-                    {
-                        id: '2_2',
-                        name: '瘦肚子舞'
-                    },
-                    {
-                        id: '2_3',
-                        name: '形体'
-                    },
-                    {
-                        id: '2_4',
-                        name: '懒人操'
-                    },
-                    {
-                        id: '2_5',
-                        name: '颈椎舞',
-                    },
-                    {
-                        id: '2_6',
-                        name: '瑜伽'
-                    },
-                    {
-                        id: '2_7',
-                        name: '全身暴汗'
-                    },
-                    {
-                        id: '2_8',
-                        name: '拍打操'
-                    },
-                ]
+                id: '1_2',
+                name: '32步'
             },
             {
-                id: 3,
-                firstCateName: '流行舞',
-                secondCate: [{
-                        id: '3_1',
-                        name: '水兵舞',
-                    },
-                    {
-                        id: '3_2',
-                        name: '鬼步舞'
-                    },
-                    {
-                        id: '3_3',
-                        name: '爵士舞'
-                    },
-                    {
-                        id: '3_4',
-                        name: '现代舞'
-                    },
-                    {
-                        id: '3_5',
-                        name: '肚皮舞',
-                    },
-                    {
-                        id: '3_6',
-                        name: '形体舞'
-                    },
-                    {
-                        id: '3_7',
-                        name: '古典舞'
-                    },
-                    {
-                        id: '3_8',
-                        name: '恰恰舞'
-                    },
-                    {
-                        id: '3_9',
-                        name: '拉丁舞'
-                    },
-                ]
+                id: '1_3',
+                name: '步子舞'
             },
+            {
+                id: '1_4',
+                name: '中三舞'
+            }
+            ]
+        },
+        {
+            id: 2,
+            firstCateName: '健身',
+            secondCate: [{
+                id: '2_1',
+                name: '健身操',
+            },
+            {
+                id: '2_2',
+                name: '瘦肚子舞'
+            },
+            {
+                id: '2_3',
+                name: '形体'
+            },
+            {
+                id: '2_4',
+                name: '懒人操'
+            },
+            {
+                id: '2_5',
+                name: '颈椎舞',
+            },
+            {
+                id: '2_6',
+                name: '瑜伽'
+            },
+            {
+                id: '2_7',
+                name: '全身暴汗'
+            },
+            {
+                id: '2_8',
+                name: '拍打操'
+            },
+            ]
+        },
+        {
+            id: 3,
+            firstCateName: '流行舞',
+            secondCate: [{
+                id: '3_1',
+                name: '水兵舞',
+            },
+            {
+                id: '3_2',
+                name: '鬼步舞'
+            },
+            {
+                id: '3_3',
+                name: '爵士舞'
+            },
+            {
+                id: '3_4',
+                name: '现代舞'
+            },
+            {
+                id: '3_5',
+                name: '肚皮舞',
+            },
+            {
+                id: '3_6',
+                name: '形体舞'
+            },
+            {
+                id: '3_7',
+                name: '古典舞'
+            },
+            {
+                id: '3_8',
+                name: '恰恰舞'
+            },
+            {
+                id: '3_9',
+                name: '拉丁舞'
+            },
+            ]
+        },
         ],
-        activityList: [{
-                id: 1,
-                title: "2021中华广场舞大赛",
-                imgUrl: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/images/index/activity.jpg?sign=49b4b4168a4ddc8cd71b237c70e3b89d&t=1654149148'
-            },
-            {
-                id: 2,
-                title: "2022湖南省广场舞大赛",
-                imgUrl: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/images/index/activity1.jpg?sign=dc68f592377d12320f50ef27a557b8e2&t=1654149159'
-            },
-            {
-                id: 3,
-                title: "社区广场舞大赛",
-                imgUrl: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/images/index/activity2.jpg?sign=42b40c63f95a8b43619210f98604aec0&t=1654149170'
-            },
-            {
-                id: 4,
-                title: "2022全国广场舞总决赛",
-                imgUrl: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/images/index/activity3.jpg?sign=934b593975dad174b29d2e6e03b463a9&t=1654149181'
-            },
-        ],
+        actList: [],
+        dianzan: false,
+        dianzanSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/dianzan.png',
+        shoucang: false,
+        shoucangSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/shoucang.png',
         swiperIndex: '1/4',
         topPic: [],
         tapID: 1, // 判断是否选中
         navHeight: ''
     },
 
-    onLoad: function() {
+    onLoad: function () {
         this.setData({
             navHeight: App.globalData.navHeight,
         });
+        this.getopenid();
         this.getVideo();
-        this.random()
+        this.random();
+        this.getAct();
     },
 
-    onShow: function() {
+    onShow: function () {
         if (App.globalData.tapID) {
             this.setData({
                 tapID: App.globalData.tapID,
@@ -158,11 +147,11 @@ Page({
     },
 
     // headerBar 点击
-    headerTitleClick: function(e) {
+    headerTitleClick: function (e) {
         this.setData({ tapID: e.target.dataset.id })
     },
 
-    handleSwiperChange: function(e) {
+    handleSwiperChange: function (e) {
         this.setData({
             swiperIndex: `${e.detail.current + 1}/4`
         })
@@ -176,16 +165,94 @@ Page({
     },
     getVideo() {
         wx.cloud.callFunction({
-                name: 'getVideo'
-            })
+            name: 'getVideo'
+        })
             .then(res => {
                 this.setData({
-                        videoList: res.result.data
-                    })
-                    // console.log('获取视频数据库成功', res.result.data)
+                    videoList: res.result.data
+                })
+                // console.log('获取视频数据库成功', res.result.data)
             })
             .catch(res => {
                 console.log('获取视频数据库失败', res)
+            })
+    },
+    getAct() {
+        wx.cloud.database().collection('activities').get()
+            .then(res => {
+                this.setData({
+                    actList: res.data
+                })
+                console.log('获取活动数据库成功', res.data)
+            })
+            .catch(err => {
+                console.log('获取活动数据库失败', err)
+            })
+    },
+    dianzanClick() {
+        if (this.data.dianzan) {
+            this.setData({
+                dianzan: false,
+                dianzanSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/dianzan.png'
+            })
+        } else {
+            this.setData({
+                dianzan: true,
+                dianzanSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/%E7%82%B9%E8%B5%9E.png'
+            })
+        }
+    },
+    shoucangClick(e){
+        if (this.data.shoucang) {
+            this.setData({
+                shoucang: false,
+                shoucangSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/shoucang.png'
+            })
+        } else {
+            this.setData({
+                shoucang: true,
+                shoucangSrc: 'https://636c-cloud1-4g8zgsp8753a10d4-1311372251.tcb.qcloud.la/icons/dynamic/%E6%94%B6%E8%97%8F.png',
+                active:e.currentTarget.dataset.id
+            })
+            this.data.actArr.push(this.data.active);
+            wx.cloud.database().collection('user')
+            .doc(this.data.openid)
+            .update({
+                data:{
+                    active:this.data.actArr
+                }
+            })
+            .then(res=>{
+                console.log('修改成功',res)
+            })
+            .catch(res=>{
+                console.log('修改失败',res)
+            })
+        }
+    },
+    getopenid() {
+        wx.cloud.callFunction({
+            name: 'getOpenid'
+        }).then(res => {
+            this.setData({ openid: res.result.openid });
+            this. getUserAct();
+            console.log('获取openid函数成功', res.result.openid);
+        }).catch(res => {
+            console.log('获取openid函数失败', res)
+        });
+    },
+    getUserAct(){
+        wx.cloud.database().collection('user')
+            .doc(this.data.openid)
+            .get()
+            .then(res=>{
+                this.setData({
+                    actArr:res.data.active
+                })
+                console.log('获取登录用户信息成功',res)
+            })
+            .catch(res=>{
+                console.log('获取登录用户信息失败',res)
             })
     }
 
